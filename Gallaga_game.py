08066f81e53c8ef6ded905eed3fb28edcace7fe0
Sen670 <smahon769@gmail.com>
2025-03-29 12:30:21 +0000

    Add files via upload

--- Gallaga_game.py
+++ Gallaga_game.py
@@ -8,6 +8,7 @@ ship = Actor("spaceship")
 ship.pos = (200,400)
 enemies = []
 bullets = []
+score = 0 
 
 for i in range(8):
     for j in range(4):
@@ -28,11 +29,43 @@ def draw():
         i.draw()
         
     ship.draw() 
+ 
+    for i in bullets:
+        i.draw()
     
 
+def on_key_down(key):
+    if key == keys.SPACE:
+        bullets.append(Actor("bullet"))
+        bullets[-1].x = ship.x 
+        bullets[-1].y = ship.y - 100
+        
 
+def display_score():
+        screen.draw.text("Score:"+str(score),(100,200),color = "white",fontsize = 50)
+
+def game_over(): 
+     global score
+     screen.draw.text("GAME OVER! Your final score is:",(score))
+     
+def update():
+     if keyboard.left:
+          ship.x -= 10
+     
+     if keyboard.right:
+          ship.x += 10
+
+     for i in bullets:
+          if i.y <= 0:
+               bullets.remove(i)
+          else:
+             i.y -= 10
+          
 
 
    
+
+
+
 
 pgzrun.go()   
